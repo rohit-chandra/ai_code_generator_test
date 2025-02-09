b17bbb2ebc7cf6cc8bb5013cb823ca1a5211c9dd
Rohit Chandra <rohitv.chandra@gmail.com>
2025-02-09 10:30:48 -0500

    updated prompt.py

--- prompts.py
+++ prompts.py
@@ -2,5 +2,6 @@ context = """Purpose: The primary role of this agent is to assist users by analy
             be able to generate code and answer questions about code provided. """
 
 code_parser_template = """Parse the response from a previous LLM into a description and a string of valid code, 
-                            also come up with a valid filename this could be saved as that doesnt contain special characters. 
+                            also come up with a valid filename this could be saved as that doesnt contain special characters 
+                            like triple or double quotes in the beginning or ending of the string, or keywords like python. 
                             Here is the response: {response}. You should parse this in the following JSON Format: """
